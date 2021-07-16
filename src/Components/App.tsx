b9059249945da1e39f37b5e487189467b46df196
Daniel Santos <iam@w-b.dev>
2021-07-16 18:24:44 -0300

    add markup with mock data and basic filtering

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
 function App() {
-  return (
-    <h1 className="App">
-      Empty
-    </h1>
-  );
+    const currentSHA = '456c'
+    const targetSHA = '789'
+    const log = [
+        {sha: '123', message: 'first commit'},
+        {sha: '456', message: 'second commit'},
+        {sha: '456b', message: 'b - second commit'},
+        {sha: '456c', message: 'c - second commit'},
+        {
+        sha: '789',
+        message: 'most recent commit'
+    }]
+    return (
+        <main className="App">
+            <div><label htmlFor="currentSHA">Current SHA:</label><input id="currentSHA" type="text" disabled
+                                                                        value={currentSHA}/></div>
+            <div><label htmlFor="targetSHA">Target SHA:</label><input id="targetSHA" type="text"
+                                                                      placeholder={targetSHA}/></div>
+            <ol style={{display: 'flex', flexDirection: 'column-reverse'}}>
+                {log.slice(log.findIndex(entry => entry.sha === currentSHA ) + 1, log.findIndex(entry => entry.sha === targetSHA ) + 1).map(entry => (<li>{entry.message}(#{entry.sha})</li>))}
+            </ol>
+        </main>
+    );
 }
 
 export default App;
